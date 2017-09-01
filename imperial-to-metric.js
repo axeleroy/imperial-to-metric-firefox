e145577bc3e98a6323132b19beeacfea67f5723d
Axel Leroy <blackeco@free.fr>
2017-09-01 13:18:33 +0200

    Added inches and yards

--- imperial-to-metric.js
+++ imperial-to-metric.js
@@ -34,7 +34,7 @@ function inches_to_meters(inch) {
 function foot_to_meters(foot) {
     return foot * 0.3048;
 }
-function yard_foot_to_meters(yard){
+function yard_to_meters(yard){
     return yard * 0.9144;
 }
 function miles_to_meters(miles) {
@@ -43,6 +43,16 @@ function miles_to_meters(miles) {
 // endregion
 
 // region regexps
+var inch_matches = [
+    "\\d+.?inches",
+    "\\d+\\.\\d+.?inches",
+    "\\d+\\.\\d+.?inch",
+    "\\d+.?inch",
+    "\\d+\\.\\d+.?in",
+    "\\d+.?in"
+];
+var inch_regexp = new RegExp(inch_matches.join('|'), "gi");
+
 var feet_matches = [
     "\\d+.?feet",
     "\\d+\\.\\d+.?feet",
@@ -53,22 +63,48 @@ var feet_matches = [
 ];
 var feet_regexp = new RegExp(feet_matches.join('|'), "gi");
 
+var yard_matches = [
+    "\\d+.?yards",
+    "\\d+\\.\\d+.?yards",
+    "\\d+\\.\\d+.?yard",
+    "\\d+.?yard",
+    "\\d+\\.\\d+.?yds",
+    "\\d+.?yds",
+    "\\d+\\.\\d+.?yd",
+    "\\d+.?yd"
+];
+var yard_regexp = new RegExp(yard_matches.join('|'), "gi");
+
 var miles_matches = [
     "\\d+\\.\\d+.?miles",
     "\\d+,\\d+.?miles",
     "\\d+.?miles",
     "\\d+\\.\\d+.?mile",
     "\\d+,\\d+.?mile",
-    "\\d+.?mile"
+    "\\d+.?mile",
+    "\\d+\\.\\d+.?ml",
+    "\\d+.?ml"
 ]
 var miles_regexp = new RegExp(miles_matches.join('|'), "gi");
 // endregion
 
 function replace(node) {
+    // inches
+    node.nodeValue = node.nodeValue.replace(inch_regexp, function(str) {
+            var val = str.match(/\d+\.\d+|\d+/g);
+            var meters = inches_to_meters(val[0]);
+            return format_length(str, meters);
+        })
     // feet
     node.nodeValue = node.nodeValue.replace(feet_regexp, function(str) {
         var val = str.match(/\d+\.\d+|\d+/g);
         var meters = foot_to_meters(val[0]);
+        return format_length(str, meters);
+    })
+    // yard
+    node.nodeValue = node.nodeValue.replace(yard_regexp, function(str) {
+        var val = str.match(/\d+\.\d+|\d+/g);
+        var meters = yard_to_meters(val[0]);
         return format_length(str, meters);
     })
     // miles
